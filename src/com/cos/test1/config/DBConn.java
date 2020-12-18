670e9c2911a7a8aa4ed9704c8147dbecdd2f592d
eunho <eunho3854@gmail.com>
2020-12-18 17:03:34 +0900

    웹서버와 DB 연결

---
+++ src/com/cos/test1/config/DBConn.java
@@ -0,0 +1,24 @@
+package com.cos.test1.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConn {
+	public static Connection getConnection() {
+		Connection conn = null;
+		String url = "jdbc:mysql://localhost:3306/ssar?serverTimezone=Asia/Seoul";
+		String username = "ssar";
+		String password = "bitc5600";
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection(url, username, password);
+			
+			System.out.println("DB 연결 성공");
+			return conn;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("DB 연결 실패");
+		return null;
+	}
+}
